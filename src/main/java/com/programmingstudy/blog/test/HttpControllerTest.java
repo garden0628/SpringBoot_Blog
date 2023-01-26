2bf37a181d689672a1d6af8d49c475ff9b0d6485
garden <bill990628@gmail.com>
2023-01-26 22:25:42 +0900

    Setting lombok

--- src/main/java/com/programmingstudy/blog/test/HttpControllerTest.java
+++ src/main/java/com/programmingstudy/blog/test/HttpControllerTest.java
@@ -5,16 +5,28 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HttpControllerTest {
 
-    // http;//localhost:8080/http/get(select)
+    private static final String TAG = "HttpControllerTest : ";
+
+    @GetMapping("/http/lombok")
+    public String lombokTest(){
+        Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+        System.out.println(TAG + "getter : " + m.getUsername());
+        m.setUsername("garden");
+        System.out.println(TAG + "setter : " + m.getUsername());
+        return "lombok test 완료";
+    }
+
+    // 인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다.
+    // http://localhost:8080/http/get(select)
     @GetMapping("/http/get")
-    public String getTest(){
-        return "get요청";
+    public String getTest(Member m){
+        return "get요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword();
     }
 
     // http://localhost:8080/http/post(insert)
     @PostMapping("/http/post")
-    public String postTest(){
-        return "post요청";
+    public String postTest(@RequestBody Member m){
+        return "post요청 : " + m.getId() + ", " + m.getUsername() + ", " + m.getPassword();
     }
 
     // http://localhost:8080/http/put(update)
